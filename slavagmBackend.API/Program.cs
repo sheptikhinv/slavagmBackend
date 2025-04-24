443656eafe8835e94cc85358c3a1d7ea8425c389
shept <sheptikhinv@gmail.com>
2025-04-24 21:47:03 +0500

    Program.cs

--- slavagmBackend.API/Program.cs
+++ slavagmBackend.API/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using slavagmBackend.API.Middlewares;
+using slavagmBackend.Infrastructure.Data;
+using slavagmBackend.Services;
+
 namespace slavagmBackend.API;
 
 public class Program
@@ -5,26 +13,83 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        AuthOptions.MakeOptions(builder.Configuration);
 
         // Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = AuthOptions.Issuer,
+                    
+                    ValidateAudience = true,
+                    ValidAudience = AuthOptions.Audience,
+
+                    ValidateLifetime = true,
+                    
+                    IssuerSigningKey = AuthOptions.SymmetricSecurityKey,
+                    ValidateIssuerSigningKey = true,
+                };
+            });
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme.",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = JwtBearerDefaults.AuthenticationScheme
+                        }
+                    },
+                    []
+                }
+            });
+        });
+
+        
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite(
+                builder.Configuration.GetConnectionString("SqliteConnection")));
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwagger(options => { options.RouteTemplate = "api/swagger/{documentName}/swagger.json"; });
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/api/swagger/v1/swagger.json", "Survey Backend V1");
+                options.RoutePrefix = "api/swagger";
+            });
         }
+        
+        app.UseMiddleware<ExceptionsMiddleware>();
 
-        app.UseHttpsRedirection();
-
+        app.UseAuthentication();
         app.UseAuthorization();
+        
+        app.MapControllers();
         
         app.Run();
     }
