85696a06e647cf588bb9a0e359c8b5969496db99
shept <sheptikhinv@gmail.com>
2025-04-24 21:47:01 +0500

    messed up models

--- slavagmBackend.Core/Models/Card.cs
+++ slavagmBackend.Core/Models/Card.cs
@@ -5,7 +5,6 @@ public class Card
     public long Id { get; set; }
     
     public required string Title { get; set; }
-    public int Priority { get; set; }
-    public Skill? Parent { get; set; }
-    public ICollection<Skill> Children { get; set; } = new  List<Skill>();
+    public required string Description { get; set; }
+    public string? Link { get; set; }
 }
--- slavagmBackend.Core/Models/Skill.cs
+++ slavagmBackend.Core/Models/Skill.cs
@@ -5,6 +5,7 @@ public class Skill
     public long Id { get; set; }
     
     public required string Title { get; set; }
-    public required string Description { get; set; }
-    public string? Link { get; set; }
+    public int Priority { get; set; }
+    public Skill? Parent { get; set; }
+    public ICollection<Skill> Children { get; set; } = new  List<Skill>();
 }
